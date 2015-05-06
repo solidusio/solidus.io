dac1b553a2445f672863b88ba47249b38541e28d
Gray Gilmore <graygilmore@gmail.com>
2015-05-06 11:58:02 -0700

    Add middleman-deploy

--- Gemfile
+++ Gemfile
@@ -1,6 +1,7 @@
 # If you do not have OpenSSL installed, update
 # the following line to use "http://" instead
-source 'https://rubygems.org'
+source "https://rubygems.org"
 
 gem "middleman", "~>3.3.12"
+gem "middleman-deploy", "~> 1.0"
 gem "bourbon"
--- config.rb
+++ config.rb
@@ -20,43 +20,30 @@
 # proxy "/this-page-has-no-template.html", "/template-file.html", :locals => {
 #  :which_fake_page => "Rendering a fake page with a local variable" }
 
-###
-# Helpers
-###
-
-# Automatic image dimensions on image_tag helper
-# activate :automatic_image_sizes
-
-# Reload the browser automatically whenever files change
-# configure :development do
-#   activate :livereload
-# end
-
-# Methods defined in the helpers block are available in templates
-# helpers do
-#   def some_helper
-#     "Helping"
-#   end
-# end
-
 set :css_dir, 'stylesheets'
 set :js_dir, 'javascripts'
 set :images_dir, 'images'
 set :fonts_dir, 'fonts'
 
+activate :deploy do |deploy|
+  deploy.method = :git
+end
+
 # Build-specific configuration
 configure :build do
-  # For example, change the Compass output style for deployment
-  # activate :minify_css
+  set :sass, {
+    style: :compressed,
+    line_comments: false
+  }
 
   # Minify Javascript on build
-  # activate :minify_javascript
+  activate :minify_javascript
 
   # Enable cache buster
   # activate :asset_hash
 
   # Use relative URLs
-  # activate :relative_assets
+  activate :relative_assets
 
   # Or use a different image path
   # set :http_prefix, "/Content/images/"
