beba7a678c1f3cc850183f571cca2b716cba58ba
Gray Gilmore <graygilmore@gmail.com>
2015-05-07 10:52:03 -0700

    Set a default partials folder

--- config.rb
+++ config.rb
@@ -19,7 +19,7 @@
 # Proxy pages (https://middlemanapp.com/advanced/dynamic_pages/)
 # proxy "/this-page-has-no-template.html", "/template-file.html", :locals => {
 #  :which_fake_page => "Rendering a fake page with a local variable" }
-
+set :partials_dir, 'partials'
 set :css_dir, 'stylesheets'
 set :js_dir, 'javascripts'
 set :images_dir, 'images'
