976c5aa7ada5d69817b700537e8bfc1477d8edda
James Gayfer <james@stembolt.com>
2018-04-16 11:38:16 -0700

    Gracefully finish heartbeat animation
    Previously, the animation would instantly reset if the user moved their
    mouse away from the heart. With these changes, the animation will
    competely finish the current iteration, even if the user moves the mouse
    away in the middle of the animation.

---
+++ source/assets/javascripts/heart.js
@@ -0,0 +1,22 @@
+window.onload = function() {
+
+  heart = document.querySelector('.heart')
+
+  function addAnimation() {
+    heart.classList.add('animate-heart')
+  }
+
+  function removeAnimation() {
+    heart.classList.remove('animate-heart')
+    heart.removeEventListener('animationiteration', removeAnimation);
+    heart.removeEventListener('webkitAnimationIteration', removeAnimation);
+  }
+
+  function removeAnimationAfterCurrentIteration () {
+    heart.addEventListener('animationiteration', removeAnimation);
+    heart.addEventListener('webkitAnimationIteration', removeAnimation);
+  }
+
+  heart.addEventListener("mouseover", addAnimation);
+  heart.addEventListener("mouseleave", removeAnimationAfterCurrentIteration);
+}
