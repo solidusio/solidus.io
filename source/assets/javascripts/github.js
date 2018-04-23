5e2c5b8c4315853098950ead3144ab841b6de4f5
James Gayfer <james@stembolt.com>
2018-04-23 15:34:39 -0700

    Render live commits from GitHub API
    By getting commit data directly from GitHub, we can display a live
    version of the latest GitHub commits. This is superior to an image as it
    will always be up to date. It will also scale much better with higher
    resolution displays as we're using elements that can be scaled by the
    browser (as opposed to a static image).

--- source/assets/javascripts/github.js
+++ source/assets/javascripts/github.js
@@ -1,3 +1,94 @@
+const COMMIT_COUNT = 10;
+const SOLIDUS_URL = 'http://github.com/solidusio/solidus'
+
+function populateCommits(json) {
+  let template = document.querySelector('.commit-template').content;
+  let commitList = document.querySelector('.commit-list');
+
+  for (let i=0; i<COMMIT_COUNT; i++) {
+    let commit = template.cloneNode(true);
+    let commit_json = json[i]
+
+    setTitle(commit, commit_json);
+    setAuthorElements(commit, commit_json);
+    setRelativeTime(commit, commit_json);
+    setSHA(commit, commit_json);
+    setCodeLink(commit, commit_json);
+    setCommitMessage(commit, commit_json);
+    setVerifiedText(commit, commit_json);
+
+    commitList.appendChild(commit);
+  }
+}
+
+function setVerifiedText(commit, commit_json) {
+  if (!commit_json["commit"]["verification"]["verified"]) {
+    commit.querySelector('.verified').style.display = 'none';
+  }
+}
+
+function setCommitMessage(commit, commit_json) {
+  let commitMessage = commit_json["commit"]["message"].split('\n\n')[1];
+  if (commitMessage) {
+    commit.querySelector('.commit-message').innerHTML = commitMessage;
+  } else {
+    commit.querySelector('.ellipsis-container').remove();
+  }
+}
+
+function setCodeLink(commit, commit_json) {
+  commit.querySelector('.code-link').href = commit_json["html_url"].replace('commit', 'tree');
+}
+
+function setSHA(commit, commit_json) {
+  commit.querySelector('.sha').innerHTML = commit_json["sha"].substring(0,7);
+  commit.querySelector('.sha').href = commit_json["html_url"];
+}
+
+function setRelativeTime(commit, commit_json) {
+  commit.querySelector('.relative-time').innerHTML = moment(commit_json['commit']['author']['date']).fromNow();
+}
+
+function setAuthorElements(commit, commit_json) {
+  commit.querySelector('.commit-author').innerHTML = commit_json["author"]["login"];
+  commit.querySelector('.commit-author').title = "View all commits by " + commit_json["author"]["login"];
+  commit.querySelector('.commit-author').href = SOLIDUS_URL + "/commits?author=" + commit_json["author"]["login"];
+  commit.querySelector('.avatar img').setAttribute("src", commit_json["author"]["avatar_url"]);
+  commit.querySelector('.avatar-link').href = commit_json["author"]["html_url"];
+}
+
+function setTitle(commit, commit_json) {
+  if (commit_json['committer']['login'] === 'web-flow') {
+    setWebFlowTitle(commit, commit_json);
+    commit.querySelector('.user-commit').remove();
+  } else {
+    setUserTitle(commit, commit_json);
+    commit.querySelector('.web-flow-commit').remove();
+  }
+}
+
+function setUserTitle(commit, commit_json) {
+  commit.querySelector('.user-commit a').innerHTML = commit_json["commit"]["message"].split('\n\n')[0];
+  commit.querySelector('.user-commit a').href = commit_json["html_url"];
+  commit.querySelector('.user-commit a').title = commit_json["commit"]["message"];
+}
+
+function setWebFlowTitle(commit, commit_json) {
+  let issueRegex = /#[0-9]+/;
+  let issue = issueRegex.exec(commit_json["commit"]["message"])[0];
+
+  commit.querySelector('.action').innerHTML = commit_json["commit"]["message"].split(issue)[0];
+  commit.querySelector('.action').title = commit_json["commit"]["message"];
+  commit.querySelector('.action').href = commit_json["html_url"];
+
+  commit.querySelector('.issue').innerHTML = issue;
+  commit.querySelector('.issue').href = SOLIDUS_URL + "/pull/" + issue.substring(1);
+
+  commit.querySelector('.branch').innerHTML = commit_json["commit"]["message"].split(issue)[1].split('\n\n')[0];
+  commit.querySelector('.branch').title = commit_json["commit"]["message"];
+  commit.querySelector('.branch').href = commit_json["html_url"];
+}
+
 function toggleCommitMessage(el) {
   commitMsg = findAncestor(el, "commit").querySelector(".commit-message");
   commitMsg.classList.toggle("hide");
@@ -7,3 +98,12 @@ function findAncestor (el, cls) {
   while ((el = el.parentElement) && !el.classList.contains(cls));
   return el;
 }
+
+let http = new XMLHttpRequest();
+http.onreadystatechange = function() {
+  if (this.readyState == 4 && this.status == 200) {
+    populateCommits(JSON.parse(this.responseText));
+  }
+}
+http.open('GET', 'https://api.github.com/repos/solidusio/solidus/commits', true);
+http.send();
