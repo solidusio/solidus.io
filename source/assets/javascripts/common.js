63eea6c54ad8debfd1dd90b145be9435c13ff461
Michela Frecchiami <michela.frecchiami@gmail.com>
2020-09-07 13:13:03 +0200

    Convert brands animation to a JS carousel

--- source/assets/javascripts/common.js
+++ source/assets/javascripts/common.js
@@ -153,6 +153,26 @@ $(function () {
     slidesToShow: 1,
     fade: true
   });
+  
+  $('.animated-logos').slick({
+    slidesToShow: 6,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 1500,
+    arrows: false,
+    dots: false,
+    responsive: [{
+        breakpoint: 768,
+        settings: {
+            slidesToShow: 4
+        }
+    }, {
+        breakpoint: 520,
+        settings: {
+            slidesToShow: 3
+        }
+    }]
+  });
 });
 
 $(document).on('aos:in', function(event) {
