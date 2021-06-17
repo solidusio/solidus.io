d0f0ffaf79a50c7f8fa5780f35bee14aea9b3fa0
Michela Frecchiami <michelafrecchiami@nebulab.it>
2021-06-17 13:51:52 +0200

    Add the Most Popular category filter

--- source/assets/javascripts/extensions.js
+++ source/assets/javascripts/extensions.js
@@ -1,8 +1,10 @@
 $(function () {
   let $filter = $(".extensions-filter__input"),
       $groups = $(".extensions-group"),
-      $cards = $(".extensions-list-block .extension-info"),
+      $cards = $(".extensions-list-block .extensions-group:not(.most-popular) .extension-info"),
       $alert = $(".extensions-list-block .alert"),
+      $mostPopular = $(".extensions-list-block .extensions-group.most-popular"),
+      $mostPopularFilter = $("#filter-most-popular"),
       selectedFilters = [];
 
 
@@ -19,16 +21,15 @@ $(function () {
 
     $groups.each(function () {
       $(this).toggleClass("empty", ($(this).find(".extension-info:not(.deselected)").length === 0));
+      $mostPopular.addClass("d-none");
     });
-
+  
     $alert.toggleClass("d-none", ($groups.filter(":not(.empty)").length > 0));
   });
 
-  //var $hiddenGroups = $( ".extensions-filter__navbar__nav .nav-item:nth-last-of-type(-n+7):hidden");
-
-  // $(".js-more").click(function() {
-  //   $hiddenGroups.slice(0, 5).slideDown();
-  // });
+  $mostPopularFilter.click(function () {
+    $mostPopular.toggleClass("selected");
+  });
 
   var $hiddenGroups = $( ".extensions-filter__navbar__nav .nav-item:nth-last-of-type(-n+7)");
 
